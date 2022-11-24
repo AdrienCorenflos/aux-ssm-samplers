0171c1a64df73e1003fd7fa4935c93c90173c15a
adriencorenflos <adrien.corenflos@gmail.com>
2022-11-24 17:54:28 +0200

    Adding some files

---
+++ aux_samplers/examples/README.md
@@ -0,0 +1,3 @@
+Original examples from the article are collected in this folder. The list (as well as rough descriptions) is given below.
+
+### LGSSM
---
+++ setup.py
@@ -0,0 +1,30 @@
+import sys
+
+import setuptools
+
+
+# READ README.md for long description on PyPi.
+try:
+    long_description = open("README.md", encoding="utf-8").read()
+except Exception as e:
+    sys.stderr.write(f"Failed to read README.md:\n  {e}\n")
+    sys.stderr.flush()
+    long_description = ""
+
+
+setuptools.setup(
+    name="aux-ssm-samplers",
+    author="Adrien Corenflos",
+    description="Auxiliary samplers for state-space models with tractable densities",
+    long_description=long_description,
+    version="0.1",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "jax>=0.3.25",
+        "jaxlib>=0.3.25",
+    ],
+    long_description_content_type="text/markdown",
+    keywords="probabilistic state space bayesian statistics sampling algorithms",
+    license="MIT",
+    license_files=("LICENSE",),
+)
