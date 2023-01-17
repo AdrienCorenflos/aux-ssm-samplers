a6f3b76296dff58fc47e71d7f4e7eddf537d590e
Adrien Corenflos <adrien.corenflos@aalto.fi>
2023-01-17 15:56:28 +0200

    Split parallel kalman in shell

--- aux_samplers/examples/stochastic_volatility/experiment.sh
+++ aux_samplers/examples/stochastic_volatility/experiment.sh
@@ -2,11 +2,13 @@ for T in 50 100 200; do
   for D in 5 30 100; do
     for style in kalman-1 kalman-2; do
       JAX_PLATFORMS=cpu python experiment.py --style $style --T $T --D $D --no-parallel --no-gpu
+      sleep 1
     done
     for N in 25 100; do
       for style in csmc csmc-guided; do
         JAX_PLATFORMS=cpu python experiment.py --style $style --N $N --T $T --D $D --no-parallel --no-gpu --gradient
         JAX_PLATFORMS=cpu python experiment.py --style $style --N $N --T $T --D $D --no-parallel --no-gpu --no-gradient
+        sleep 1
       done
     done
   done
@@ -15,10 +17,11 @@ done
 
 for T in 50 100 200; do
   for D in 5 30; do
-    for style in kalman-1 kalman-2; do
-      JAX_PLATFORMS=CUDA_VISIBLE_DEVICES=0 python experiment.py --style $style --T $T --D $D --parallel --gpu
-    done
+    JAX_PLATFORMS=CUDA_VISIBLE_DEVICES=0 python experiment.py --style kalman-1 --T $T --D $D --parallel --gpu &
+    JAX_PLATFORMS=CUDA_VISIBLE_DEVICES=1 python experiment.py --style kalman-2 --T $T --D $D --parallel --gpu
+    sleep 5
     JAX_PLATFORMS=cuda CUDA_VISIBLE_DEVICES=0 python experiment.py --style csmc --N 25 --T $T --D $D --parallel --gpu --gradient &
     JAX_PLATFORMS=cuda CUDA_VISIBLE_DEVICES=1 python experiment.py --style csmc --N 25 --T $T --D $D --parallel --gpu --no-gradient
+    sleep 5
   done
 done
