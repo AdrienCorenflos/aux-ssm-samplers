The PIT cSMC works for proposals without a gradient. It's not yet clear to me why the gradient makes things worse. Could simply be a bug in the treatment of the weights. This is actually rather likely given the implementation of the F-K model is different from the original model: the initial weights are rather dissimilar.

aux_samplers/csmc/independent.py
@@ -11,7 +11,8 @@ from jax import numpy as jnp
 from jax.scipy.stats import norm
 
 from .generic import get_kernel as get_base_kernel
-from .._primitives.csmc.base import Distribution, UnivariatePotential, Dynamics, Potential
+from .._primitives.csmc.base import Distribution, UnivariatePotential, Dynamics, Potential, CSMCState
+from .._primitives.csmc.pit.csmc import get_kernel as get_pit_kernel
 
 
 def get_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Potential, N: int,
@@ -47,7 +48,14 @@ def get_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Pote
     init: Callable
         Function to initialize the state of the sampler given a trajectory.
     """
+    if not parallel:
+        return _get_classical_kernel(M0, G0, Mt, Gt, N, backward, Pt, gradient)
+    else:
+        return _get_parallel_kernel(M0, G0, Mt, Gt, N, gradient)
 
+
+def _get_classical_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Potential, N: int,
+                          backward: bool, Pt: Optional[Dynamics], gradient):
     # This function uses the classes defined below
     def factory(u, scale):
         if gradient:
@@ -55,7 +63,7 @@ def get_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Pote
         else:
             grad_pi = 0. * u
         m0 = AuxiliaryM0(u=u[0], sqrt_half_delta=scale[0], grad=grad_pi[0])
-        mt = AuxiliaryMt(params=(u[1:], scale[1:], grad_pi[1:]))
+        mt = AuxiliaryMtDynamics(params=(u[1:], scale[1:], grad_pi[1:]))
         if gradient:
             g0 = GradientAuxiliaryG0(M0=M0, G0=G0, u=u[0], sqrt_half_delta=scale[0], grad=grad_pi[0])
             gt = GradientAuxiliaryGt(Mt=Mt, Gt=Gt, params=(u[1:], scale[1:], grad_pi[1:]))
@@ -64,10 +72,49 @@ def get_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Pote
             gt = AuxiliaryGt(Mt=Mt, Gt=Gt)
         return m0, g0, mt, gt
 
-    if not parallel:
-        return get_base_kernel(factory, N, backward, Pt)
-    else:
-        raise NotImplementedError("Parallel version not implemented yet.")
+    return get_base_kernel(factory, N, backward, Pt)
+
+
+def _get_parallel_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Potential, N: int, gradient: bool):
+    def factory(u, scale):
+        if gradient:
+            grad_pi = jax.grad(_log_pdf)(u, M0, G0, Mt, Gt)
+        else:
+            grad_pi = 0. * u
+        mt = AuxiliaryMtDistribution(params=(u, scale, grad_pi))
+        if gradient:
+            g0 = GradientAuxiliaryG0(M0=M0, G0=G0, u=u[0], sqrt_half_delta=scale[0], grad=grad_pi[0])
+            gt = GradientAuxiliaryGt(Mt=Mt, Gt=Gt, params=(u[1:], scale[1:], grad_pi[1:]))
+        else:
+            g0 = AuxiliaryG0(M0=M0, G0=G0)
+            gt = AuxiliaryGt(Mt=Mt, Gt=Gt)
+
+        return mt, g0, gt
+
+    def kernel(key, state, delta):
+        # Housekeeping
+        x = state.x
+        T = x.shape[0]
+
+        sqrt_half_delta = jnp.sqrt(0.5 * delta)
+        if jnp.ndim(sqrt_half_delta) == 0:
+            sqrt_half_delta = sqrt_half_delta * jnp.ones((T,))
+        auxiliary_key, key = jax.random.split(key)
+
+        # Auxiliary observations
+        u = x + sqrt_half_delta[:, None] * jax.random.normal(auxiliary_key, x.shape)
+
+        mt, g0, gt = factory(u, sqrt_half_delta)
+
+        _, auxiliary_kernel = get_pit_kernel(mt, g0, gt, N)
+        return auxiliary_kernel(key, state)
+
+    def init(x):
+        T, *_ = x.shape
+        ancestors = jnp.zeros((T,), dtype=jnp.int_)
+        return CSMCState(x=x, updated=ancestors != 0)
+
+    return init, kernel
 
 
 def get_coupled_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, Gt: Potential, N: int,
@@ -112,7 +159,7 @@ def get_coupled_kernel(M0: Distribution, G0: UnivariatePotential, Mt: Dynamics, 
         else:
             grad_pi = 0. * u
         m0 = AuxiliaryM0(u=u[0], sqrt_half_delta=scale[0], grad=grad_pi[0])
-        mt = AuxiliaryMt(params=(u[1:], scale[1:], grad_pi[1:]))
+        mt = AuxiliaryMtDynamics(params=(u[1:], scale[1:], grad_pi[1:]))
         if gradient:
             g0 = GradientAuxiliaryG0(M0=M0, G0=G0, u=u[0], sqrt_half_delta=scale[0], grad=grad_pi[0])
             gt = GradientAuxiliaryGt(Mt=Mt, Gt=Gt, params=(u[1:], scale[1:], grad_pi[1:]))
@@ -189,12 +236,23 @@ class GradientAuxiliaryG0(UnivariatePotential):
 
 
 @chex.dataclass
-class AuxiliaryMt(Dynamics):
+class AuxiliaryMtDynamics(Dynamics):
     def sample(self, key, x_t, params):
         u_t, sqrt_half_delta, grad_t = params
         half_delta = sqrt_half_delta ** 2
         mean = u_t[None, :] + half_delta * grad_t[None, :]
         return mean + sqrt_half_delta * jax.random.normal(key, x_t.shape)
+
+@chex.dataclass
+class AuxiliaryMtDistribution(Distribution):
+    params: Array
+
+    def sample(self, key, N):
+        u_t, sqrt_half_delta, grad_t = self.params
+        d = u_t.shape[-1]
+        half_delta = sqrt_half_delta ** 2
+        mean = u_t[None, :] + half_delta * grad_t[None, :]
+        return mean + sqrt_half_delta * jax.random.normal(key, (N, d))
 
 
 @chex.dataclass

aux_samplers/examples/stochastic_volatility/experiment.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser("Run a Stochastic volatility experiment")
 # General arguments
 parser.add_argument('--parallel', action='store_true')
 parser.add_argument('--no-parallel', dest='parallel', action='store_false')
-parser.set_defaults(parallel=False)
+parser.set_defaults(parallel=True)
 parser.add_argument('--debug', action='store_true')
 parser.add_argument('--no-debug', dest='debug', action='store_false')
 parser.set_defaults(debug=False)
@@ -30,12 +30,12 @@ parser.add_argument('--no-gpu', dest='gpu', action='store_false')
 parser.set_defaults(gpu=False)
 parser.add_argument('--verbose', action='store_true')
 parser.add_argument('--no-verbose', dest='verbose', action='store_false')
-parser.set_defaults(verbose=False)
+parser.set_defaults(verbose=True)
 
 # Experiment arguments
 parser.add_argument("--n-experiments", dest="n_experiments", type=int, default=10)
 parser.add_argument("--T", dest="T", type=int, default=50)
-parser.add_argument("--D", dest="D", type=int, default=5)
+parser.add_argument("--D", dest="D", type=int, default=30)
 parser.add_argument("--n-samples", dest="n_samples", type=int, default=10_000)
 parser.add_argument("--burnin", dest="burnin", type=int, default=2_500)
 parser.add_argument("--target-alpha", dest="target_alpha", type=float, default=0.5)
@@ -43,7 +43,7 @@ parser.add_argument("--lr", dest="lr", type=float, default=1.)
 parser.add_argument("--beta", dest="beta", type=int, default=0.1)
 parser.add_argument("--delta-init", dest="delta_init", type=float, default=1e-10)
 parser.add_argument("--seed", dest="seed", type=int, default=1234)
-parser.add_argument("--style", dest="style", type=str, default="kalman-2")
+parser.add_argument("--style", dest="style", type=str, default="csmc")
 parser.add_argument("--gradient", action='store_true')
 parser.add_argument('--no-gradient', dest='gradient', action='store_false')
 parser.set_defaults(gradient=False)
@@ -68,7 +68,9 @@ m0, P0, F, Q, b = get_dynamics(NU, PHI, TAU, RHO, args.D)
 
 if args.style != "kalman":
     args.target_alpha = 1 - (1 + args.N) ** (-1 / 3)
-
+elif args.style == "csmc" and args.parallel:
+    # More conservative target alpha for parallel cSMC. This is perhaps an artifact of the warmup, but it's not clear.
+    args.target_alpha = 1 - (1 + args.N) ** (-1 / 6)
 
 # STATS FN
 def stats_fn(x_1, x_2):
@@ -170,7 +172,7 @@ def full_experiment():
     for i in tqdm.trange(args.n_experiments,
                          desc=f"Style: {args.style}, T: {args.T}, N: {args.N}, D: {args.D}, gpu: {args.gpu}"):
         start = time.time()
-        (esjd, *_), _, _, _, pct_accepted, burnin_delta = one_experiment(keys[i])
+        (esjd, traj, squared_exp), true_xs, _, xs_init, pct_accepted, burnin_delta = one_experiment(keys[i])
 
         ejsd_per_key[i, :] = esjd.block_until_ready()
         acceptance_rate_per_key[i, :] = pct_accepted
@@ -186,7 +188,19 @@ def full_experiment():
         #     time_per_key[i] = time.time() - start
         # except:  # noqa
         #     continue
+        from matplotlib import pyplot as plt
+        plt.plot(np.arange(args.T), traj[..., -1], color="tab:orange")
+        std = np.sqrt(squared_exp[..., -1] - traj[..., -1] ** 2)
+        plt.fill_between(np.arange(args.T), traj[..., -1] + 2 * std, traj[..., -1] - 2 * std,
+                         color="tab:orange", alpha=0.2)
+        plt.plot(np.arange(args.T), true_xs[:, -1], color="tab:blue")
+        plt.plot(np.arange(args.T), xs_init[:, -1], color="k", linestyle="--")
+        plt.show()
 
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(np.arange(args.T), burnin_delta, color="tab:blue")
+        ax.twinx().plot(np.arange(args.T), pct_accepted, color="tab:orange")
+        plt.show()
     return ejsd_per_key, acceptance_rate_per_key, delta_per_key, time_per_key
 
 